4ddc8f1ed9dcc68caeab70850d880efb52e14610
jrgranada <jroberto.granada@gmail.com>
2019-03-31 11:48:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Google Cloud Platform!</title>
+    </head>
+    <body>
+		<form action="script.php" method="get">
+			<h1>Probando la conexión a la BD!</h1>
+			</br>
+			</br>
+			Name: <input type="text" name="name">
+			</br>
+			</br>
+			<input type="submit" value="Registrar!">
+		</form>
+    </body>
+</html>
---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+	$usuario = "root";
+	$contrasena = "Juan4409454";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.
+	$servidor = "localhost";
+	$basededatos = "seminario";
+
+	$conexion = mysqli_connect( $servidor, $usuario, $contrasena) or die ("No se ha podido conectar al servidor de Base de datos");
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+
+	$sql = "INSERT INTO usuarios(nombre)VALUES ('".$_GET["name"]."')";
+	 
+	if (mysqli_query($conexion, $sql)) {
+		echo "El usuario '".$_GET["name"]."' se registró correctamente";
+	} else {
+		echo "Error: " . $sql . "" . mysqli_error($conexion);
+	}
+	$conexion->close();
+?>
